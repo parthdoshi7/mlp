5cc56ef2a771181fa2ecba499898bbf446adb8ea
Parth Doshi <144593310+parthdoshi7@users.noreply.github.com>
2025-01-23 20:01:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
           'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
           'Sharjah', 'Mohali', 'Bengaluru']
 
-pipe = pickle.load(open('pipe.pkl', 'rb'))
+pipe = pickle.load('pipe.pkl', 'rb')
 
 st.title('IPL Win Predictor')
 
